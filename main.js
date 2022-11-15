9c077fab02f078b83e4931eba438458536f2d9e6
cy-bug <shashwat5590@gmail.com>
2022-11-15 22:40:58 +0530

    wuhahaahha get rickrolled bitch

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var cards = [], associations = [], flipped = [];
 let first_flip = '', second_flip = '', previousTarget;
 var count = 0, delay = 800;
+const astley = true;
 var API_KEY = '31266814-2adc0eb15aa7d78852e5cb233';
 var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('minimal');
 const items = [
@@ -39,7 +40,6 @@ function kaam_shuru(size = 1) {
         let front_elm = document.createElement("div");
         card_elm.className = 'card';
         front_elm.className = 'card-front'
-        front_elm.innerText = "se";
         card_container.appendChild(card_elm);
         cards.push(card_elm);
     }
@@ -57,7 +57,7 @@ function initialize_cards() {
 
     for (let index = 0; index < associations.length; index++) {
         const elm = cards[index];
-        elm.innerText = associations[index];
+        // elm.innerText = associations[index];
     }
 }
 
@@ -107,10 +107,57 @@ function handle_flip(elm) {
     }
 }
 
+function astley_wink() {
+    let __ = cards.concat([]);
+    
+    var blink = setInterval(function() {
+        let __2 = [];
+        for (let i = 0; i < 10; i++) {
+            let index = Math.floor(Math.random()* __.length);
+            const element = __[index];
+            __2.push(element);
+        }
+        for (let index = 0; index < __2.length; index++) {
+            const element = __2[index];
+            if (element.classList.contains("astley")) {
+                element.classList.remove("astley");
+            } else {
+                element.classList.add("astley");
+            }
+        }
+    }, 600)
+}
+
+function rickroll(elm) {
+    elm.classList.add('astley');
+    let roll = document.getElementById("rickroll_audio");
+    roll.play();
+
+    let uncovered = cards.concat([]);
+    uncovered.splice(cards.indexOf(elm), 1);
+    var timer = setInterval(function() {
+        if (uncovered.length <= 0){
+            clearInterval(timer);
+            astley_wink();
+        } 
+        for (let i = 0; i < Math.floor(Math.random() * 6); i++) {
+            let x = Math.floor(Math.random()*uncovered.length);
+            let item = uncovered[x];
+            item.classList.add('astley');
+            uncovered.splice(x, 1);
+            if (uncovered.length == 0) {break;}
+        }
+    }, 400);
+}
+
 function install_listeners() {
     cards.forEach(card => {
         card.addEventListener("click", e => {
-            handle_flip(e.target);
+            if (astley){
+                rickroll(e.target);
+            } else {
+                handle_flip(e.target);
+            }
         });
     });
 
